7a276cfe39e07d7fb2c04f38e17c2f6519628a1d
huangshaoyang <hsymail@163.com>
2018-12-09 16:19:12 +0800

    first commit

---
+++ src/com/hsy/learn/Hello.java
@@ -0,0 +1,11 @@
+package com.hsy.learn;
+
+/**
+ * Created by hsy on 2018/12/9 4:17 PM
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
