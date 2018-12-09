17a8631dac72d65e7bd53fbca4faa0adb2bd9864
huangshaoyang <hsymail@163.com>
2018-12-09 16:31:28 +0800

    sec

--- src/com/hsy/learn/Hello.java
+++ src/com/hsy/learn/Hello.java
@@ -1,5 +1,8 @@
 package com.hsy.learn;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by hsy on 2018/12/9 4:17 PM
  */
@@ -7,5 +10,16 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+                "Stanislas Wawrinka",
+                "David Ferrer","Roger Federer",
+                "Andy Murray","Tomas Berdych",
+                "Juan Martin Del Potro"};
+        List<String> players =  Arrays.asList(atp);
+
+        players.forEach((player) -> System.out.print(player + "; "));
+        players.forEach(System.out::println);
     }
+
+
 }
